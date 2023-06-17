99ec7bc02265d678d7eb56d8dd7bffd55d1b51ec
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-06-17 11:01:42 +0300

    lab3

---
+++ lab3/contexts/usersContextProvider.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import React ,{createContext,useEffect,useState}from "react";
+export const usersContext=createContext();
+const UsersContextProvider = (props) => {
+    // console.warn(props)
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(res=>{
+            setUsers(res.data)
+        })
+
+        // fetch("https://jsonplaceholder.typicode.com/users")
+        // .then(res=>res.json()).then(data=>setUsers(data))
+    },[])
+    
+    return (
+       <usersContext.Provider value={{users}}>
+        {props.children}
+       </usersContext.Provider>
+
+    );
+}
+
+
+export default UsersContextProvider;
